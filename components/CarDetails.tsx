665e079b29180967eb526ab34cbf54fde85790dc
mwahk3ymer <audreymwakima99@gmail.com>
2023-08-09 19:33:34 +0300

    892023 carddetails

---
+++ components/CarDetails.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import Image from 'next/image';
+
+import { Dialog, Transition } from '@headlessui/react';
+
+import { CarProps } from '@/types';
+
+interface CarDetailsProps {
+    isOpen: boolean;
+    closeModal: () => void;
+    car: CarProps;
+}
+
+const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
+  return (
+    <div>CarDetails</div>
+  )
+}
+
+export default CarDetails
