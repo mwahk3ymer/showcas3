1daccb10655f69e4d90b0b2a6b067389c5744be6
mwahk3ymer <audreymwakima99@gmail.com>
2023-09-06 16:38:26 +0300

    9.6.2023 showmore

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from '@/constants';
 
@@ -41,13 +41,17 @@ export default async function Home({ searchParams }) {
               <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ): (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops no results</h2>
             <p>{allCars?.message}</p>
           </div>
-        
         )}
 
       </div>
